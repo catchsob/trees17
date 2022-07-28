575a40090a6763daa562764e4d85036f5f4bbb0f
catchsob <42635950+catchsob@users.noreply.github.com>
2022-07-28 23:37:24 +0800

    for tf lite

---
+++ hdf5_to_tflite.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+
+def h5_to_tfl(h5, tfl=None, res=200):
+    savedmodel = os.path.splitext(h5)[0]
+    model_h5 = tf.keras.models.load_model(h5)
+    model_h5.save(savedmodel, save_format='tf')
+    model_tf = tf.saved_model.load(savedmodel)
+    
+    concrete_func = model_tf.signatures[tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
+    if None in concrete_func.inputs[0].shape.as_list()[1:-1]:
+        concrete_func.inputs[0].set_shape([1, res, res, 3])
+    converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func])
+    tflite_model = converter.convert()
+    
+    if not tfl:
+        tfl = os.path.extsep.join([os.path.splitext(h5)[0], 'tflite'])    
+    with open(tfl, 'wb') as f:
+        f.write(tflite_model)
+        
+    return tfl
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--hdf5', type=str, required=True)
+parser.add_argument('--tflite', type=str, default=None)
+parser.add_argument('--resolution', type=int, default=200)
+
+args = parser.parse_args()
+
+a = h5_to_tfl(args.hdf5, args.tflite, args.resolution)
+if a:
+    print(f'{args.hdf5} converts to {a} done.')
+else:
+    print(f'{args.hdf5} converts failed!')
---
+++ trees-tflite.py
@@ -0,0 +1,41 @@
+import argparse
+
+import numpy as np
+from PIL import Image
+import tflite_runtime.interpreter as tflite
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--image', default='榕樹.jpg', help='image')
+    parser.add_argument('-l', '--labels', type=str, help='the file name for label-name mapping, illegal file would be ignored')
+    parser.add_argument('-m', '--model', default='trees17V1.tflite', help='.tflite model')
+    args = parser.parse_args()
+
+    interpreter = tflite.Interpreter(model_path=args.model)
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
+    img = Image.open(args.image).resize((width, height))
+    input_data = np.expand_dims(img, axis=0)
+    floating_model = input_details[0]['dtype'] == np.float32
+    if floating_model:
+        input_data = input_data.astype('float32') / 127.5 - 1
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    
+    interpreter.invoke()
+    
+    output_data = interpreter.get_tensor(output_details[0]['index'])    
+    results = np.squeeze(output_data)
+    ans = results.argsort()[-5:][::-1][0]
+    
+    if args.labels:
+        try:
+            with open(args.labels, encoding='utf-8') as f:
+                ans = f.readlines()[ans].strip()
+        except:
+            pass
+    
+    print(ans)
