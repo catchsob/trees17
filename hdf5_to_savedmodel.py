a44a23b232b73b29e025759f4119c787696893da
catchsob <42635950+catchsob@users.noreply.github.com>
2022-08-28 22:15:07 +0800

    revise SAVEDMODEL to optional
    SAVEDMODEL is now optional. If missed, the HDF5 name would be the generated SavedModel path name.

--- hdf5_to_savedmodel.py
+++ hdf5_to_savedmodel.py
@@ -1,13 +1,16 @@
 import os
 import argparse
 
-from tensorflow.keras.models import load_model
-
 parser = argparse.ArgumentParser()
 parser.add_argument('HDF5', type=str)
-parser.add_argument('SAVEDMODEL', type=str)
+parser.add_argument('SAVEDMODEL', type=str, nargs='?')
 
 args = parser.parse_args()
+if not args.SAVEDMODEL:
+    args.SAVEDMODEL = os.path.splitext(args.HDF5)[0]
 if not os.path.isdir(args.SAVEDMODEL):
     os.mkdir(args.SAVEDMODEL)
+    
+from tensorflow.keras.models import load_model
+
 load_model(args.HDF5).save(filepath=args.SAVEDMODEL, save_format='tf')
