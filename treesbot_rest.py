fc1ffd47d38ae06603dd2602214efbf7a8b244e4
catchsob <42635950+catchsob@users.noreply.github.com>
2022-10-01 14:56:21 +0800

    support env.json and rename

---
+++ treesbot_rest.py
@@ -0,0 +1,84 @@
+import io
+import json
+
+import requests
+import numpy as np
+from PIL import Image, ImageOps
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage
+)
+
+app = Flask(__name__)
+
+with open('env.json') as f:
+    env = json.load(f)
+line_bot_api = LineBotApi(env['YOUR_CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(env['YOUR_CHANNEL_SECRET'])
+label = env['YOUR_LABELS']
+rest = f'http://{env["YOUR_REST_HOST"]}:{env["YOUR_REST_PORT"]}/v1/models/{env["YOUR_MODEL_NAME"]}:predict'
+res = 448
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_message(event):
+    message_id = event.message.id
+    message_content = line_bot_api.get_message_content(message_id)
+        
+    b = b''
+    for chunk in message_content.iter_content():
+        b += chunk
+    img = Image.open(io.BytesIO(b))
+    r = classify_rest(img)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=r))
+
+
+def classify_rest(img):
+    img = ImageOps.fit(img, (res, res))
+    img = np.expand_dims(img, axis=0)/255.
+    headers = {"content-type": "application/json"}
+    data = json.dumps({"instances": img.tolist()})
+    r = requests.post(rest, headers=headers, data=data)
+    p = np.argmax(r.json()['predictions'])
+    with open(label, encoding='utf-8') as f:
+        labels = f.read().split()
+    return labels[p] if 0 <= p < len(labels) else 'unknown'
+
+
+if __name__ == "__main__":
+    app.run()
